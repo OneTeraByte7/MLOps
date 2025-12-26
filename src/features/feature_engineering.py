41d36ec263942c85645394181a320ce9d0967606
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-12-26 14:46:17 +0530

    defined:create_feature

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -16,3 +16,37 @@ class FeatureEngineer:
         self.categorical_features = self.config['data']['categorical_features']
         self.target = self.config['data']['target']
         
+        
+        self.preprocessor = None
+     #derived features   
+    def create_features(self, df):
+        df = df.copy()
+        
+        df['revenue_per_user'] = df['monthly_revenue'] / df['team_size'] #revenue per team memebr
+        
+        df['engagement_score'] = (  #engagement scores
+            df['logins_per_month'] * 0.3 +
+            df['features_usage_depth'] * 100 * 0.3 +
+            (df['api_calls_per_month'] / 1000) * 0.4
+        )
+        
+        df['support_burden'] = df['support_tickets'] * df['avg_ticket_resolution_days']
+        
+        df['days_inactive_ratio'] = df['days_since_last_login'] / np.maximum(df['account_age_days'], 1)
+        
+        df['total_contract_value'] = df['monthly_revenue'] * df['contract_length_months']
+        
+        df['payment_reliability'] = 1 / (1 + df['payment_delays'])
+        
+        df['nps_category'] = pd.cut(df['nps_score'], bins = [-1, 6, 8, 10],
+                                    labels = ['Detector', 'Passive', 'Promoter'])
+        
+        self.numeric_features_extend([
+            'revenue_per_user', 'engagement_score', 'support_burden', 'days_inactive_ratio', ' total_contract_value',' payment_reliability'
+        ])
+        
+        self.categorical_features_append('nps_category')
+        
+        return df
+        
+        
